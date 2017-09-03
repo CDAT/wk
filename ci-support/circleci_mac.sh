871e49969bcf573d7e695d89a81cd34e3eb6ef8a
Charles Doutriaux <doutriaux1@llnl.gov>
2017-09-03 12:21:43 -0700

    enabling circle ci

---
+++ ci-support/circleci_mac.sh
@@ -0,0 +1,10 @@
+export UVCDAT_ANONYMOUS_LOG=False
+export PATH=${HOME}/miniconda/bin:${PATH}
+#export VCS_BACKGROUND=0  # circleci seg faults on bg=1
+python run_tests.py -n 1 -v2 -g 
+RESULT=$?
+echo "test command exit result:",$RESULT
+if [ $RESULT -eq 0 -a $CIRCLE_BRANCH == "master" ]; then conda install conda-build anaconda-client ; fi
+if [ $RESULT -eq 0 -a $CIRCLE_BRANCH == "master" ]; then bash ./ci-support/conda_upload.sh ; fi
+exit $RESULT
+
