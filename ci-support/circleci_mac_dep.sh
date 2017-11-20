ed7b6679b8ec77e01b296919424303e2271921cc
Charles Doutriaux <doutriaux1@llnl.gov>
2017-11-20 12:17:55 -0800

    first pass at py3 good to go

--- ci-support/circleci_mac_dep.sh
+++ ci-support/circleci_mac_dep.sh
@@ -2,6 +2,11 @@
 ls
 pwd
 export PATH=${HOME}/miniconda/bin:${PATH}
-conda install -c uvcdat/label/nightly -c conda-forge -c uvcdat vtk-cdat cdutil genutil dv3d vcs vcsaddons mesalib=17.1.4=3 nose image-compare flake8 matplotlib
+conda create -q -n py3 -c uvcdat/label/nightly -c nadeau1  -c conda-forge -c uvcdat "cdms2>2.12.2017" nose flake8 "python>3" mesalib image-compare "matplotlib<2.1" numpy=1.13 "vcs>2.12.2017"
+conda create -q -n py2 -c uvcdat/label/nightly -c conda-forge -c uvcdat "cdms2>2.12.2017" nose flake8 mesalib image-compare "matplotlib<2.1" numpy=1.13 "vcs>2.12.2017"
 export UVCDAT_ANONYMOUS_LOG=False
+source activate py2
+python setup.py install
+source activate py3
+rm -rf build
 python setup.py install
