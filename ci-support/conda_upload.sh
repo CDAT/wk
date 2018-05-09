09cfa53b54645db148082130615b1a18e04590c3
Muryanto <muryanto1@ML-9516678.llnl.gov>
2018-05-09 09:41:17 -0700

    migrate MacOS build to CircleCI 2.0 -- updating conda_upload.sh

--- ci-support/conda_upload.sh
+++ ci-support/conda_upload.sh
@@ -22,9 +22,8 @@ mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=${HOME}/conda-bld
 echo "Cloning recipes"
-git clone git://github.com/UV-CDAT/conda-recipes
+git clone git://github.com/CDAT/conda-recipes
 cd conda-recipes
-# uvcdat creates issues for build -c uvcdat confises package and channel
 python ./prep_for_build.py
 conda build $PKG_NAME -c cdat/label/nightly -c conda-forge -c cdat 
 conda build $PKG_NAME -c cdat/label/nightly -c ${ESMF_CHANNEL} -c conda-forge -c cdat --python=3.6
