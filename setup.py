8f5797b6dc3803f65eab18b323edfc9cf477cb16
Charles Doutriaux <doutriaux1@llnl.gov>
2018-09-04 17:47:24 -0700

    adding tutorial

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup (name = "WK",
        version='8.0',
        description = "Package to compute and draw the Wheeler-Kiladis figures",
-       url = "http://cdat.sf.net, see WK 99",
+       url = "http://github.com/cdat/wk",
        packages = ['WK'],
        package_dir = {'WK': 'lib'},
        data_files = [("share/wk",["share/test_data_files.txt"])],
